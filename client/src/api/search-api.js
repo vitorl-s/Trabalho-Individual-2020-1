b541db0f7b71795cde5ced680f609bd3a7b6f8cf
Vitor Leal <vitor_leal2201@hotmail.com>
2020-12-10 21:23:15 -0300

    Test duplicates
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- client/src/api/search-api.js
+++ client/src/api/search-api.js
@@ -8,4 +8,22 @@ const SearchAPI = {
   }
 };
 
+const SearchAPI = {
+  searchProfiles(query) {
+    return API.get(`${endpointRoot}/?query=${query}`)
+  }
+};
+
+const SearchAPI = {
+  searchProfiles(query) {
+    return API.get(`${endpointRoot}/?query=${query}`)
+  }
+};
+
+const SearchAPI = {
+  searchProfiles(query) {
+    return API.get(`${endpointRoot}/?query=${query}`)
+  }
+};
+
 export default SearchAPI
