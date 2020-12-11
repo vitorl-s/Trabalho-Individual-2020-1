4fc15f2491464b3db7adfa1b5e0472c3386a8298
Vitor Leal <vitor_leal2201@hotmail.com>
2020-12-10 21:17:22 -0300

    test code climate integration
    Signed-off-by: Vitor Leal <vitor_leal2201@hotmail.com>

--- client/src/api/search-api.js
+++ client/src/api/search-api.js
@@ -3,9 +3,9 @@ import API from './api'
 const endpointRoot = '/search'
 
 const SearchAPI = {
-  searchProfiles (query) {
+  searchProfiles(query) {
     return API.get(`${endpointRoot}/?query=${query}`)
   }
-}
+};
 
 export default SearchAPI
